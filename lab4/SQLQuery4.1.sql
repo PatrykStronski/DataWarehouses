6cc100fd4da15483616f845cd1e4b6f1fce65144
Patryk Stronski <p.stronski13@gmail.com>
2019-11-26 18:05:40 +0100

    commit

--- lab4/SQLQuery4.1.sql
+++ lab4/SQLQuery4.1.sql
@@ -1,18 +1,21 @@
 USE AdventureWorks2014;
-
+DROP TABLE DIMProductRatio
 CREATE TABLE DIMProductRatio(
 	ProductID INTEGER, 
 	Rating FLOAT, 
-	City VARCHAR(100), 
+	Country VARCHAR(100), 
 	Continent VARCHAR(100), 
 	Gender VARCHAR(100),
-	RDate DATE,
-	[RYear] AS YEAR(RDate), 
-	[RMonth] AS MONTH(RDate), 
-	[RDay] AS DAY(RDate)
+	RDate VARCHAR(11),
+	[RYear] INTEGER, 
+	[RMonth] INTEGER, 
+	[RDay] INTEGER
 );
 
-INSERT INTO DIMProductRatio(ProductID,Rating,City,Continent,Gender,RDate) SELECT 
-"PID", "Rating", "Country", r.Continent, "gender", Cast("date" as datetime) 
+SELECT "date" FROM "data-l4-rating";
+
+INSERT INTO DIMProductRatio(ProductID,Rating,Country,Continent,Gender,RDate,RYear,RMonth,RDay) SELECT 
+r.PID, r.Rating, r.Country, c.Continent, r.gender, dd.Style101,dd.year,dd.month,dd.day
 FROM "data-l4-rating" r
-INNER JOIN "data-l4-continent" c ON c.ContinentId = r.Continent;
+INNER JOIN "data-l4-continent" c ON c.ContinentId = r.Continent
+INNER JOIN dimDate dd ON dd.Style101 = r.Date;
--- lab4/SQLQuery4.2.sql
+++ lab4/SQLQuery4.2.sql
@@ -2,4 +2,4 @@ SELECT TOP 1 category, Rating, COUNT (dp.ProductID)
 FROM dimProduct dp
 INNER JOIN DIMProductRatio dpr ON dpr.ProductID =  dp.productId
 GROUP BY category, Rating
-ORDER BY COUNT(dp.ProductID);
+ORDER BY COUNT(dp.ProductID), Rating DESC;
